059897707d26da809f815a1f5c860881a8e4b3e6
PatelAbhay550 <patelabhay550@gmail.com>
2023-04-30 09:20:18 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/cricstory/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.cricstory.app
+
+import com.cricstory.app.appcomponents.base.BaseActivity
+import com.cricstory.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
